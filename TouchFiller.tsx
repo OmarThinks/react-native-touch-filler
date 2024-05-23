33b3e9edb3aaaf52e2f4947ff38845f613a6535b
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2024-05-23 23:49:33 +0300

    TouchFiller: progress

--- TouchFiller.tsx
+++ TouchFiller.tsx
@@ -1,12 +1,47 @@
-import { Pressable } from "react-native";
-import { memo } from "react";
-import type { GestureResponderEvent } from "react-native";
+import { Pressable, StyleSheet } from "react-native";
+import { memo, useMemo } from "react";
+import type { GestureResponderEvent, StyleProp, ViewStyle } from "react-native";
 import React from "react";
+const emptyFunction = () => {
+  console.log("Pressed");
+};
 
-React.ComponentProps;
+type TouchFillerProps = React.ComponentProps<typeof Pressable> & {
+  onPress?:
+    | null
+    | undefined
+    | React.ComponentProps<typeof Pressable>["onPress"];
+  borderRadius?: number;
+  style?: StyleProp<ViewStyle>;
+};
 
-type TouchFillerProps = React.ComponentProps<typeof Pressable>;
+const TouchFiller = memo(
+  ({
+    style,
+    onPress = emptyFunction,
+    borderRadius,
+    ...props
+  }: TouchFillerProps) => {
+    if (onPress === null) {
+      return null;
+    }
 
-const TouchFiller = memo(({ style, onPress, ...props }: TouchFillerProps) => {
-  return <Pressable onPress={} />;
+    const _style: StyleProp<ViewStyle> = useMemo(() => {
+      return StyleSheet.flatten([styles.pressable, style]);
+    }, [style]);
+
+    return <Pressable {...props} onPress={onPress} style={[_style, style]} />;
+  }
+);
+
+const styles = StyleSheet.create({
+  pressable: {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+    overflow: "hidden",
+  },
 });
+
+export type { TouchFillerProps };
+export default TouchFiller;
